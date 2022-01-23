69073272d0a93b51f92083b6bf9e6607b4f7295c
Brian Holbrook <brian.holbrook56@gmail.com>
2022-01-23 17:06:36 -0600

    all done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dayPlanner
+
+Day Planner is a lightweight work day to-do list. it presents the current time of day at the top of the page and provides a to-do list within regular 9-5 business hours. the list will grey out prior hours of the day, red for the current hour and green for future hours. when the save button is clicked that hour gets saved locally to the browser incase the user wants to return.
+
+link
+----
+
+A [link](https://holbrookb23.github.io/dayPlanner/)
+
+screenshot
+----------
+
+![Image](./assets/dayPlanner.png)
---
+++ assets/script.js
@@ -0,0 +1,47 @@
+//global variables
+
+
+//functions
+function saveTask() {
+    console.log($(this).parent());
+    
+    const hourTime = $(this).parent().attr("id");
+    const hourTask = $(this).siblings("textarea").val();
+    
+    localStorage.setItem(hourTime, hourTask);
+}
+//event listeners
+function setColors() {
+    let currentHour = moment().hour();
+   
+    $(".js-row").children("textarea").each(function() {
+        const row = $(this);
+        const divHour = row.parent().data("hour");
+        if (divHour < currentHour) {
+            $(this).addClass("past");
+        } else if (divHour === currentHour) {
+            $(this).addClass("present");           
+        } else {
+            $(this).addClass("future");
+        }
+    }) 
+}
+
+//show current date
+window.setInterval(function() {
+    $('#currentDay').text(moment().format("hh:mm:ss MMMM Do, YYYY"));
+}, 1000);
+//grab tasks from local storage
+
+$("#h9 .js-task").val(localStorage.getItem("h9"));
+$("#h10 .js-task").val(localStorage.getItem("h10"));
+$("#h11 .js-task").val(localStorage.getItem("h11"));
+$("#h12 .js-task").val(localStorage.getItem("h12"));
+$("#h13 .js-task").val(localStorage.getItem("h13"));
+$("#h14 .js-task").val(localStorage.getItem("h14"));
+$("#h15 .js-task").val(localStorage.getItem("h15"));
+$("#h16 .js-task").val(localStorage.getItem("h16"));
+$("#h17 .js-task").val(localStorage.getItem("h17"));
+//save task
+$('.js-saveBtn').on("click", saveTask);
+setColors();
